375713eb26b028130db75d5dc391b2d43579e857
Sharwan Kumar <sharwank@stg.com>
2024-06-05 12:00:08 +0530

    first jenkins file commit

---
+++ JavaProject/src/main/java/com/stg/coding/JavaProjectApplication.java
@@ -0,0 +1,14 @@
+package com.stg.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+		System.out.println("Dhak-dhak go");
+	}
+
+}
